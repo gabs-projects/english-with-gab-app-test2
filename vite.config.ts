97c44d7079648062040b89945dc58d26fde5ddc4
gabs-projects <gabi.martinsg13@gmail.com>
2026-01-12 21:05:04 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Isso evita o erro de 'process is not defined' no navegador
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env': {}
+  },
   base: './',
   build: {
     outDir: 'dist',
